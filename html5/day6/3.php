6c621e9b6cc3b182f567772f3468a614032f254e
horry77 <42237194+horry77@users.noreply.github.com>
2019-04-18 05:17:29 +0900

    Create 3.php

---
+++ html5/day6/3.php
@@ -0,0 +1,21 @@
+<html>
+
+        <head>
+
+                <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+
+        </head>
+
+        <body>
+
+                <?php
+
+                echo $_REQUEST['nickname'].'님의 직업은 '.$_REQUEST['first'].'이군요!';
+
+                ?>
+
+        </body>
+
+</html>
+
+
